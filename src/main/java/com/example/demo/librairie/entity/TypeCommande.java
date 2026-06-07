b530362c8f813a6bc01a3a39635b2f47b319645a
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-06-07 22:37:37 +0300

    feat(livraison) : add entity livraison & enum statutLivraaison

---
+++ src/main/java/com/example/demo/librairie/entity/TypeCommande.java
@@ -0,0 +1,5 @@
+package com.example.demo.librairie.entity;
+
+public enum TypeCommande {
+    DELIVERY, DOWNLOAD
+}
--- src/main/java/com/example/demo/librairie/entity/TypeCommande.kt
+++
@@ -1,5 +0,0 @@
-package com.example.demo.librairie.entity
-
-enum class TypeCommande {
-    DELIVERY, DOWNLOAD
-}
