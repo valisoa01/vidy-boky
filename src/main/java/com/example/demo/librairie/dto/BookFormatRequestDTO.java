7b3846033b29fb9a5806ce841c705d13e7037ffc
Lahatranomena <hei.lahatra.3@gmail.com>
2026-06-10 08:27:02 +0300

    feat: add crud of BookFormat

--- src/main/java/com/example/demo/librairie/dto/BookFormatRequestDTO.java
+++ src/main/java/com/example/demo/librairie/dto/BookFormatRequestDTO.java
@@ -3,14 +3,13 @@ package com.example.demo.librairie.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookFormatRequestDTO {
-    private Integer bookId;
-    private Integer formatId;
-    private BigDecimal price;
+    private UUID bookId;
+    private UUID formatId;
+    private Double price;
 }
--- src/main/java/com/example/demo/librairie/dto/BookFormatResponseDTO.java
+++ src/main/java/com/example/demo/librairie/dto/BookFormatResponseDTO.java
@@ -1,20 +1,20 @@
 package com.example.demo.librairie.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
+import java.util.UUID;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class BookFormatResponseDTO {
-    private Integer id;
-    private Integer bookId;
+    private UUID id;
+    private UUID bookId;
     private String bookTitle;
-    private Integer formatId;
+    private UUID formatId;
     private String typeFormat;
-    private BigDecimal price;
+    private Double price;
 }
