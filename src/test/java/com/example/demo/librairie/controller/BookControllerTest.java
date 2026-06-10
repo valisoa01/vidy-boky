d4130180f303b783f66f5ef631fc9c70ad3a1ef0
valisoa01 <valisoatolotriniaina@gmail.com>
2026-06-10 11:58:24 +0300

    add test connection

--- src/test/java/com/example/demo/librairie/controller/BookControllerTest.java
+++ src/test/java/com/example/demo/librairie/controller/BookControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BookController.class)
+@ActiveProfiles("test")
 class BookControllerTest {
 
     @Autowired
