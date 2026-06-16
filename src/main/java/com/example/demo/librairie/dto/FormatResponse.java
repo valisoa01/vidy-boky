f5ffa941389ffc9ee261c8f7e83192cf1a95d518
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-06-16 08:53:26 +0300

    feat(format) : add endpoint to format

---
+++ src/main/java/com/example/demo/librairie/dto/FormatResponse.java
@@ -0,0 +1,11 @@
+package com.example.demo.librairie.dto;
+
+import java.util.UUID;
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class FormatResponse {
+
+    private UUID id;
+    private String formatType;
+}
--- src/main/java/com/example/demo/librairie/repository/FormatRepository.java
+++ src/main/java/com/example/demo/librairie/repository/FormatRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.librairie.repository;
 
 import com.example.demo.librairie.entity.Format;
+
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FormatRepository extends JpaRepository<Format, UUID> {}
+public interface FormatRepository extends JpaRepository<Format, UUID> {
+    Optional<Format> findByFormatType(String formatType);
+    boolean existsByFormatType(String formatType);
+}
