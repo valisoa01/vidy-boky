e784b9c28685c1750f70f149364ab57af38f8ffc
valisoa01 <valisoatolotriniaina@gmail.com>
2026-06-18 21:47:18 +0300

    add all

--- src/main/java/com/example/demo/librairie/controller/GenreController.java
+++ src/main/java/com/example/demo/librairie/controller/GenreController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/genre")
+@RequestMapping("/api/genres")
 @RequiredArgsConstructor
 public class GenreController {
 
